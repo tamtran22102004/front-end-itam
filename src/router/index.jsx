731c76614ea6c2e56be1302681d04c0299b68bec
tamtran22102004 <iloveyou2210vpa@gmail.com>
2025-10-13 15:39:10 +0700

    fix path

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -12,14 +12,14 @@ import AttributePage from "../pages/Attribute";
 import CategoryAttributePage from "../pages/CategoryAttribute";
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "",
     element: <AppLayout />,
     children: [
-      { path: "home", element: <HomePage /> },
-      { path: "login", element: <LoginPage /> },
-      { path: "register", element: <RegisterPage /> },
+      { path: "/home", element: <HomePage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/register", element: <RegisterPage /> },
       {
-        path: "category",
+        path: "/category",
         element: (
           <PrivateRoute>
             <CategoryPage />
@@ -27,7 +27,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "category/attribute",
+        path: "/category/attribute",
         element: (
           <PrivateRoute>
             <AttributePage />
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "category/categoryattribute",
+        path: "/category/categoryattribute",
         element: (
           <PrivateRoute>
             <CategoryAttributePage />
